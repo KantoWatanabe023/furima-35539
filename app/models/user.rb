1b393f9f0915f1e5102bbe486dc54d89921c27e9
KantoWatanabe023 <dash9air6k@icloud.com>
2021-05-23 17:19:46 +0900

    新規投稿、ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :email, inclusion: { in: ["@"] }
+  validates :nickname, presence: true
+  # # パスワードが確認用一致しているか検証
+  # validates :encrypted_password, confirmation: true
+  # validates :encrypted_password_confirmation
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_detail, presence: true
+  validates :first_name_detail, presence: true
+  validates :birth_day, presence: true
+
+
 end
